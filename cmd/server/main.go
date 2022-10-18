78ae8aa8d051ba7ad9a6648ca17b564b42407417
Lucas Simon R. Magalhaes <ext.lucas.magalhaes@dafiti.com.br>
2022-10-18 19:09:24 -0300

    Setup

---
+++ cmd/server/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
+	"github.com/lucassrmagalhaes/ecommerce/configs"
+	_ "github.com/lucassrmagalhaes/ecommerce/docs"
+	_ "github.com/lucassrmagalhaes/ecommerce/internal/entity"
+	_ "github.com/lucassrmagalhaes/ecommerce/internal/infra/database"
+	_ "github.com/lucassrmagalhaes/ecommerce/internal/infra/webserver/handlers"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// @title           Go Expert API Example
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Wesley Willians
+// @contact.url    http://www.fullcycle.com.br
+// @contact.email  atendimento@fullcycle.com.br
+
+// @license.name   Full Cycle License
+// @license.url    http://www.fullcycle.com.br
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JwtExperesIn", configs.JwtExperesIn))
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
+
+	http.ListenAndServe(":8000", r)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucassrmagalhaes/ecommerce
+
+go 1.19
